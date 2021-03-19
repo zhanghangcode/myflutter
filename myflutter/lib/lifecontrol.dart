108ecdd26bd2b0409b12c2cfb0a6359fe0ab2d51
user <codezlegend@gmail.com>
2021-03-19 21:05:24 +0900

    error修正

--- myflutter/lib/lifecontrol.dart
+++ myflutter/lib/lifecontrol.dart
@@ -40,22 +40,7 @@ class _CounterWidgetState extends State<CounterWidget>{
         title: Text("子树中获取State对象"),
       ),
       body: Center(
-        child: Builder(builder: (context) {
-          return RaisedButton(
-            onPressed: () {
-              // 查找父级最近的Scaffold对应的ScaffoldState对象
-              ScaffoldState _state = context.ancestorStateOfType(
-                  TypeMatcher<ScaffoldState>());
-              //调用ScaffoldState的showSnackBar来弹出SnackBar
-              _state.showSnackBar(
-                SnackBar(
-                  content: Text("我是SnackBar"),
-                ),
-              );
-            },
-            child: Text("显示SnackBar"),
-          );
-        }),
+        child: Text(""),
       ),
     );
   }
